f6a88daf2a533b510675c920b76041ae5b85816d
Julia Prianti <juliaprianti3382@hotmail.com>
2025-08-21 14:20:18 -0300

    RI1

---
+++ index.js
@@ -0,0 +1,328 @@
+const personagem1 = {
+  nome: "Tony Stark",
+  codinome: "Homem de Ferro",
+  armaPrincipal: "Armadura",
+  armaSecundaria: "",
+  velocidade: 80,
+  forca: 85,
+  resistencia: 80,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem2 = {
+  nome: "Peter Parker",
+  codinome: "Homem Aranha",
+  armaPrincipal: "Teia",
+  armaSecundaria: "Sentido Aranha",
+  velocidade: 80,
+  forca: 65,
+  resistencia: 70,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem3 = {
+  nome: "Natasha Romanoff",
+  codinome: "Viuva Negra",
+  armaPrincipal: "Bastões de Eletrochoque",
+  armaSecundaria: "",
+  velocidade: 70,
+  forca: 85,
+  resistencia: 90,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem4 = {
+  nome: "Bruce Banner",
+  codinome: "Hulk",
+  armaPrincipal: "Força",
+  armaSecundaria: "Inteligência",
+  velocidade: 60,
+  forca: 95,
+  resistencia: 95,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem5 = {
+  nome: "Thor Odinson",
+  codinome: "Thor",
+  armaPrincipal: "Martelo",
+  armaSecundaria: "Força",
+  velocidade: 95,
+  forca: 98,
+  resistencia: 85,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem6 = {
+  nome: "Wanda Maxinoff",
+  codinome: "Feiticeira Escarlate",
+  armaPrincipal: "Magia do Caos",
+  armaSecundaria: "Manipulação da Realidade",
+  velocidade: 70,
+  forca: 70,
+  resistencia: 60,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem7 = {
+  nome: "Clint Barton",
+  codinome: "Gavião Arqueiro",
+  armaPrincipal: "Arco e flecha",
+  armaSecundaria: "",
+  velocidade: 70,
+  forca: 65,
+  resistencia: 60,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagem8 = {
+  nome: "Scott Lang",
+  codinome: "Homem Formiga",
+  armaPrincipal: "Crescimento e encolhimento",
+  armaSecundaria: "",
+  velocidade: 80,
+  forca: 75,
+  resistencia: 70,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const vilao = {
+  nome: "Thanos",
+  codinome: "Thanos",
+  armaPrincipal: "Manopla do Infinito",
+  armaSecundaria: "",
+  velocidade: 80,
+  forca: 90,
+  resistencia: 80,
+  descricao: function () {
+    return (
+      "Nome do Personagem: " +
+      this.nome +
+      "\n" +
+      "Codinome do Personagem: " +
+      this.codinome +
+      "\n" +
+      "Arma Principal: " +
+      this.armaPrincipal +
+      "\n" +
+      "Arma Secundaria: " +
+      this.armaSecundaria +
+      "\n" +
+      "Nivel de Velocidade: " +
+      this.velocidade +
+      "\n" +
+      "Nivel de Força: " +
+      this.forca +
+      "\n" +
+      "Nivel de Resistência: " +
+      this.resistencia
+    );
+  },
+};
+const personagens = [
+  personagem1,
+  personagem2,
+  personagem3,
+  personagem4,
+  personagem5,
+  personagem6,
+  personagem7,
+  personagem8,
+  vilao,
+];
+function Maisforte(lista) {
+  let vencedor = lista[0];
+  let maior = vencedor.forca + vencedor.velocidade + vencedor.resistencia;
+
+  for (let indice = 1; indice < lista.length; indice++) {
+    let personagemAtual = lista[indice];
+    let totalPontos = personagemAtual.forca + personagemAtual.velocidade + personagemAtual.resistencia;
+
+    if (totalPontos > maior) {
+      vencedor = personagemAtual;
+      maior = totalPontos;
+    }
+  }
+
+  console.log("O vencedor é");
+  console.log(vencedor.descricao());
+  console.log("Total de pontos:", maior);
+}
+
+Maisforte(personagens);
